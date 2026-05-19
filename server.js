4421f2a25b1fa58b07ca02d34f5c84673e0ed35a
genez75016 <genez75016@gmail.com>
2026-05-19 03:26:41 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+  const express = require('express')
+  const app = express()
+  app.use(express.static(__dirname))
+  app.listen(process.env.PORT || 3000)
+
+  Crée package.json :
+  {
+    "name": "app",
+    "scripts": { "start": "node server.js" },
+    "dependencies": { "express": "*" }
+  }
